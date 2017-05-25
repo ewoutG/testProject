2a17812ce1b0ce573105b8d135f4eb8cea4602d9
ewoutG <inception5@hotmail.be>
2017-05-25 12:17:04 +0200

    added material

--- TestProject/src/app/app.module.ts
+++ TestProject/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MdButtonModule, MdCheckboxModule} from "@angular/material";
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [MdButtonModule, MdCheckboxModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
